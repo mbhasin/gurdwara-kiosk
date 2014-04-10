01573be4419a6350221b270147a3ab426042dc03
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-10 15:03:18 +0100

    Can tell if answered correctly

--- public/js/supersant.js
+++ public/js/supersant.js
@@ -10,19 +10,16 @@ function getQuestion(){
 function checkAnswer(){
   $('body').on('click', '.s-option', function(event){
     event.preventDefault();
-    console.log($(this).text());
-    // var question = $('.q-no').text() - 1;
-    // $.getJSON( "json/supersant.json", function(data) {
-    //   var correct = data[question].correct;
-    //   console.log(correct);
-    //   console.log($(this));
-    //   if ($(this).text() === correct) {
-    //     console.log(correct);
-    //     console.log($(this).text());
-    //   }else{
-    //     console.log("incorrect");
-    //   };
-    // });
+    var answer = $(this).text();
+    var question = $('.q-no').text() - 1;
+    $.getJSON( "json/supersant.json", function(data) {
+      var correct = data[question].correct
+      if(answer === correct){
+        console.log("Correct!!!!");
+      }else{
+        console.log("Incorrect!!!!");
+      }
+    });
   });
 }
 
