dbc938fc0ef6d3cfc2067f41f5c28db47e4df065
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-04-10 07:14:15 +0100

    Fixed file structure

--- controllers/application.rb
+++ controllers/application.rb
@@ -1,7 +1,49 @@
 get '/' do
-  erb :index
+  erb :index, :layout => false
 end
 
 get '/getstarted' do
   erb :getstarted
+end
+
+get '/contact' do
+  erb :contact
+end
+
+get '/etiquette' do
+  erb :etiquette
+end
+
+get '/events' do
+  erb :events
+end
+
+get '/news' do
+  erb :news
+end
+
+get '/program' do
+  erb :program
+end
+
+#etiquette
+
+get '/etiquette-general' do
+  erb :"/etiquette/general"
+end
+
+get '/etiquette-langar' do
+  erb :"/etiquette/langar"
+end
+
+get '/etiquette-prayer' do
+  erb :"/etiquette/prayer"
+end
+
+get '/etiquette-sanskaar' do
+  erb :"/etiquette/sanskaar"
+end
+
+get '/etiquette-wedding' do
+  erb :"/etiquette/wedding"
 end
